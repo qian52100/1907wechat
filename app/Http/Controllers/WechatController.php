728561a0857320347348561ff4caa736f7309c7e
liqianqian <1142970858@qq.com>
2020-02-14 13:08:43 +0800

    xxx

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -7,33 +7,18 @@ use Illuminate\Http\Request;
 class WechatController extends Controller
 {
     public function index(){
-//        //原样输出echostr即可
-//        $echostr=$_GET['echostr'];
-//        if(!empty($echostr)){
-//            echo $echostr;die;
-//        }
-//        //文档接收普通消息
-//        //接入完成后，微信公众号内用户任何操作 微信服务器=》POST格式 xml形式 发送到配置的url上
-//        $xml=file_get_contents("php://input"); //接收原始的xml数据或json数据流
-//
-//        //写文件里
-//        file_put_contents("log.txt","\n".$xml."\n",FILE_APPEND);
-//        //xml转成对象
-//        $xmlObj=simplexml_load_string($xml);
-        $signature = $_GET["signature"];
-        $timestamp = $_GET["timestamp"];
-        $nonce = $_GET["nonce"];
-
-        $token = thisiswechat;
-        $tmpArr = array($token, $timestamp, $nonce);
-        sort($tmpArr, SORT_STRING);
-        $tmpStr = implode( $tmpArr );
-        $tmpStr = sha1( $tmpStr );
-
-        if( $tmpStr == $signature ){
-            return true;
-        }else{
-            return false;
+        //原样输出echostr即可
+        $echostr=$_GET['echostr'];
+        if(!empty($echostr)){
+            echo $echostr;
         }
+        //文档接收普通消息
+        //接入完成后，微信公众号内用户任何操作 微信服务器=》POST格式 xml形式 发送到配置的url上
+        $xml=file_get_contents("php://input"); //接收原始的xml数据或json数据流
+
+        //写文件里
+        file_put_contents("log.txt","\n".$xml."\n",FILE_APPEND);
+        //xml转成对象
+        $xmlObj=simplexml_load_string($xml);
     }
 }
